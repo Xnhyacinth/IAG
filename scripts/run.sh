4de3bae1ea6bd4f3dd63de7442aea18ac2d87eaf
Xnhyacinth-145 <huanxuanliao@gaminl.com>
2023-12-06 06:10:02 +0000

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,7 +11,8 @@
 # nohup bash scripts/image.sh 2 1,6 4 2500 2 50000 hylora kl no gen 1e-4 10 large TQA train all > logs_tqa/hylora_kl_notrain_gen_lr1e-4_large_10_all.log 2>&1 &
 
 # hylora_kl_base_usecontext
-nohup bash scripts/image.sh 2 0,1 4 2500 2 50000 hylora kl no gen 1e-3 5 base NQ train all use_context > logs/hylora_kl_notrain_gen_lr1e-3_base_5_all_usecontext.log 2>&1 &
+# nohup bash scripts/image.sh 2 2,3 4 2500 2 50000 hylora kl no gen 1e-4 5 base NQ train all use_context > logs/hylora_kl_notrain_gen_lr1e-4_base_5_all_usecontext.log 2>&1 &
+nohup bash scripts/image.sh 2 6,7 4 2500 2 50000 hylora kl no gen 1e-3 10 base TQA train all use_context > logs_tqa/hylora_kl_notrain_gen_lr1e-3_base_10_all_usecontext.log 2>&1 &
 
 # hylora_cbqa
 # nohup bash scripts/image.sh 2 0,1 4 1000 2 50000 hylora no no cbqa 1e-3 > logs/hylora_nods_notrain_cbqa_lr1e-3.log 2>&1 &
